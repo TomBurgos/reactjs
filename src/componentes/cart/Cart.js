2a32d1faf2fbc4653c79edd17c73247ef5555fe0
TomBurgos <87551264+TomBurgos@users.noreply.github.com>
2022-01-28 19:24:07 -0300

    Delete  src/componentes directory

--- src/componentes/cart/Cart.js
+++
@@ -1,9 +0,0 @@
-
-const Cart = () => {
-  return(
-      <div>
-          <h4>Carrito Vacio...</h4>
-      </div>
-  )
-}
-export default Cart;
--- src/componentes/footer/Footer.js
+++
@@ -1,9 +0,0 @@
-
-const Footer = () => {
-    return(
-     <footer>
-         <p> Copyright 2021 &copy; Tomas Burgos  </p>
-     </footer>
-)
-}
-export default Footer;
